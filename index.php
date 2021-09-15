37960a4ff683e4c7b420b87b2e53aef89688639a
Gustavo <80364735+ogustavomendes@users.noreply.github.com>
2021-09-15 17:22:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Facebook – entre ou cadastre-se</title>
+	<link rel="icon" type="image/x-icon" href="https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-3-1.png">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <style>
+    	body{
+			background-color: #f0f2f5;
+		}
+		input.login-form{
+			width: 90%;
+			height: 50px;
+			margin: 7px;
+			background: transparent;
+			box-shadow: 0px 0px 2px #e6e6e6;
+			border: 1px solid #e6e6e6 !important;
+			border-radius: 5px;
+			color: #888;
+			font-family: inherit;
+			font-size: 17px;
+			padding-left: 20px;
+		}
+		button{
+			width: 90%;
+			height: 50px;
+			margin: 4px;
+			border-radius: 5px;
+			border: none;
+			background-color: #1877f2;
+			color: white;
+			font-family: inherit;
+			line-height: 48px;
+			margin-bottom: 15px;
+		}
+		div.content-form{
+			box-shadow: 0px 0px 15px #e6e6e6;
+			background-color: white;
+			width: 75%;
+			height: 350px;
+			padding-top: 15px;
+			border-radius: 10px;
+
+		}
+		span.forgot{
+			color: blue;
+			font-family: helvetica;
+			font-size: 14px;
+			font-weight: 200pt;
+		}
+		button.create{
+			width: 40%;
+			height: 50px;
+			margin: 4px;
+			border-radius: 5px;
+			border: none;
+			background-color: #42b72a;
+			color: white;
+			font-family: helvetica;
+			line-height: 48px;
+			margin-bottom: 15px;
+		}
+		span{
+			font-family: inherit;
+			font-size: 13px;
+		}
+		div.col-6{
+			margin-top: 130px;
+		}
+		h3{
+			font-family: helvetica;
+		}
+		img{
+			margin-top: 50px;
+		}
+    </style>
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<!-- div left -->
+			<div class="col-6">
+				<img width="60%" src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg">
+				<h3 style="margin: 30px; margin-top: 0px !important;">O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</h3>
+			</div>
+			<!-- div right -->
+			<div class="col-6">
+				<center>
+					<div class="content-form">
+						<center>
+							<form>
+								<input class="login-form" placeholder="Email or Phone Number" type="email" name="mail"><br>
+								<input class="login-form" placeholder="Password" type="passsword" name="password"><br>
+								<button>Entrar</button>
+
+								<span style="cursor: pointer;" class="forgot">Esqueceu a sua senha?</span>
+
+								<hr style="width: 90%">
+
+								<button class="create">Criar nova Conta</button>
+							</form>
+						</center>
+					</div>
+					<div style="margin: 20px;">
+					<span><b style="cursor: pointer;">Criar uma Página</b> para uma celebridade, banda ou empresa.</span>
+					</div>
+				</center>
+			</div>
+		</div>
+	</div>
+
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</body>
+</html>
